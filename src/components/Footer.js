bd51167551eec946fc87e7c3d246bddb7e3836f7
Bmb Tech <hanchobmb@gmail.com>
2026-05-01 16:24:43 +0300

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+
+export const Footer = () => {
+  return (
+    <footer>
+        <footer className="bg-white  shadow dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-2xl p-4 md:flex md:items-center md:justify-between">
+              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2026 <Link to="/" className="hover:underline">Bmb Tech 🍿</Link>. All Rights Reserved.
+            </span>
+            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <a href="https://www.instagram.com/bmbxmd" target="_blank" rel="noreferrer"  className="hover:underline me-4 md:me-6">Instagram</a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/bmbtech/" target="_blank" rel="noreferrer" className="hover:underline me-4 md:me-6">LinkedIn</a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/bmbxmd/" target="_blank" rel="noreferrer" className="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/bmbxmd/" target="_blank" rel="noreferrer" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+            </div>
+</footer>
+
+    </footer>
+  )
+}
